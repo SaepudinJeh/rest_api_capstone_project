c14d5942d9de56b6a5bfc2daa16e0e754a883e3c
Saepudin <agung.saepudin@ist.id>
2022-06-06 21:37:27 +0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('Fitness API')
+    .setDescription('The API Fitness')
+    .setVersion('0.0.1')
+    .addTag('Fitness', 'API')
+    .build();
+
+  const documentSwagger = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('api', app, documentSwagger);
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
