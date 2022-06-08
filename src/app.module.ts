3e922b642c2c005d15d8c298cb450cbc0d7eb3ec
Saepudin <agung.saepudin@ist.id>
2022-06-08 19:30:43 +0700

    add module databases

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FitnessModule } from './fitness-module/fitness.module';
-import { DatabaseModule } from './databases/mongoose.module';
+import { DatabaseModule } from './database.module/mongoose.module';
 import { CloudinaryModule } from './cloudinary.module/cloudinary.module';
 
 @Module({
